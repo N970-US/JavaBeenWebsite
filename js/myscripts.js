76f83dd75882a7b1710ec25caa9b47d8f523460f
N970-US <70176521+N970-US@users.noreply.github.com>
2023-05-17 18:30:22 -0400

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,14 +1,13 @@
-const email = document.getElementById('email')
-const message = document.getElementById('message')
-const form = document.getElementById
-const errorElement = document.getElementById('error')
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', e => {
     let messages = []
     if (messages.value.length >= 300) {
         message.push('Message must be shorter than 300 characters')
-        e.preventDefault()
-        error
+
     }
 
 })
