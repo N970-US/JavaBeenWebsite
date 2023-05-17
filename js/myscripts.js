e31a450394d10ee057a460cd0a172b594d690fe2
N970-US <70176521+N970-US@users.noreply.github.com>
2023-05-17 18:18:22 -0400

    Updated Contact Page Java Script

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,1 +1,15 @@
-// This is the main Javascript file
+const email = document.getElementById('email')
+const message = document.getElementById('message')
+const form = document.getElementById
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (messages.value.length >= 300) {
+        message.push('Message must be shorter than 300 characters')
+        e.preventDefault()
+        error
+    }
+
+})
+
