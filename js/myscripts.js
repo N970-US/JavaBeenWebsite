8e1a2aa792d02da63d75ee76b02c49e54ece2a58
N970-US <70176521+N970-US@users.noreply.github.com>
2023-05-17 18:47:51 -0400

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,14 +1,46 @@
-const email = document.getElementById('email');
-const message = document.getElementById('message');
 const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
 const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', e => {
-    let messages = []
-    if (messages.value.length >= 300) {
-        message.push('Message must be shorter than 300 characters')
+    e.preventDefault();
 
+    validateInputs();
+
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const validateInputs = () => {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const subjectValue = subject.value.trim();
+    const messageValue = message.value.trim();
+
+    if(nameValue === '') {
+        setError(name, 'Name is Required');
+    } else {
+        setSuccess(name);
     }
-
-})
+};
 
