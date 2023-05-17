a021f9bf3d5c56768ef71fa44b2557f4c15dfcbc
N970-US <70176521+N970-US@users.noreply.github.com>
2023-05-17 19:34:45 -0400

    Test

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,6 +1,3 @@
-submitBtn.addEventListener('click', validate);
-
-
 const submitBtn = document.getElementById('submit-btn');
 
 const validate = (e) => {
@@ -8,17 +5,28 @@ const validate = (e) => {
   const username = document.getElementById('username');
   const emailAddress = document.getElementById('email-address');
   if (username.value === "") {
-    alert("USERNAME IS  NULLL");
+    alert("Please enter your username.");
     username.focus();
     return false;
   }
+    
   if (emailAddress.value === "") {
-    alert("Please enter your email address.ESTTESTTES");
+    alert("Please enter your email address.");
+    emailAddress.focus();
+    return false;
+  }
+
+  if (!emailIsValid(emailAddress.value)) {
+    alert("Please enter a valid email address.");
     emailAddress.focus();
     return false;
   }
   
-  return true;
+  return true; // Can submit the form data to the server
+}
+
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
 submitBtn.addEventListener('click', validate);
